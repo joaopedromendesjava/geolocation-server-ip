4973cba2cb7100a83da7b141db12332d8bc1882d
joaopedromendesjava <joaopedromendessilva@gmail.com>
2025-12-22 20:27:23 -0300

    fix: validate ip address

--- src/main/java/com/ip/geolocation/service/domain/valueobject/IpAddress.java
+++ src/main/java/com/ip/geolocation/service/domain/valueobject/IpAddress.java
@@ -1,11 +1,13 @@
 package com.ip.geolocation.service.domain.valueobject;
 
+import lombok.Builder;
 import org.apache.commons.validator.routines.InetAddressValidator;
 
+@Builder
 public record IpAddress(String ip) {
 
     public IpAddress {
-        if (InetAddressValidator.getInstance().isValid(ip)) {
+        if (!InetAddressValidator.getInstance().isValid(ip)) {
             throw new IllegalArgumentException("Ip not valid " + ip);
         }
     }
